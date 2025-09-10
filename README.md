45d89d952a2330acd384c6c47d73b0dfaa496204
karunrajkumar16 <your-real-email@example.com>
2025-09-10 08:36:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<!-- Run the code -->
+python main.py
+
+<!-- Test the code -->
+python test.py
---
+++ main.py
@@ -0,0 +1,12 @@
+def to_upper(name):
+    return name.upper()
+
+
+def say_hello(name):
+   return print(f'Hello,{name}')
+
+if __name__ == "__main__":
+    name = "TrainWithSatrunjay"
+    say_hello(name)
+    up = to_upper(name)
+    print(up)
